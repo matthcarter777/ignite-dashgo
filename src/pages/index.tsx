d7d109db4ab758cc7171e85368afc188c1169110
Mateus <mateushbsb@gmail.com>
2022-05-11 20:50:37 -0300

    IGNITE - 009: Validate forms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
 import { Button, Flex, Stack } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
 import { Input } from '../components/Form/Input';
 
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório.').email('E-mail inválido.'),
+  password: yup.string().required('Senha obrigatória.'),
+})
+
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  });
+
+  const { isSubmitting, errors } = formState;
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setInterval(resolve, 2000));
+  }
+
   return (
     <Flex
       w="100vw"
@@ -17,12 +41,35 @@ export default function Home() {
         p="8"
         borderRadius="8"
         flexDirection="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack>
-          <Input name="email" label="email"  type="email"/>
-          <Input name="password" label="Senha" type="password"/>
+          
+          <Input
+            name="email" 
+            type="email" 
+            label="E-mail"
+            error={errors.email}
+            {...register('email')}
+          />
+
+          <Input
+            name="password" 
+            type="password" 
+            label="Senha"
+            error={errors.password} 
+            {...register('password')}
+          />
         </Stack>
-        <Button type="submit"  mt="6" colorScheme="pink" size="lg">Entrar</Button>
+        <Button 
+          type="submit"  
+          mt="6" 
+          colorScheme="pink" 
+          size="lg"
+          isLoading={isSubmitting} 
+        >
+            Entrar
+          </Button>
       </Flex>
     </Flex>
   )
