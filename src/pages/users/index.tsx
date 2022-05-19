7cbd35ff7c788b483b97a991b2e7be9917f6f6eb
Mateus <mateushbsb@gmail.com>
2022-05-19 20:07:14 -0300

    IGNITE - 009: create hook getUsers use react-query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,42 +1,19 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { useQuery } from 'react-query';
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    
-    const response = await fetch('http://localhost:3000/api/users');
-    const data = await response.json();
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        created_at: new Date(user.created_at).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric',
-         })
-      }
-    })
-
-    return users;
-
-  });
-
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
-
-
 
   return (
     <Box>
@@ -47,7 +24,10 @@ export default function UserList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+              {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />  }
+            </Heading>
             <Link href="users/create" passHref>
               <Button 
                 as="a" 
@@ -116,13 +96,8 @@ export default function UserList() {
                 </>
               )
             }
-
-          
-
         </Box>
-
       </Flex>
-
     </Box>
   )
 }
