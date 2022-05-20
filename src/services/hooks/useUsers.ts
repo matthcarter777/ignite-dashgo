48804e25cccab8f97300445f33240d557490e136
Mateus <mateushbsb@gmail.com>
2022-05-19 21:56:25 -0300

    IGNITE - 012:  get users whith pagination

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -8,27 +8,38 @@ type User = {
   created_at: string;
 }
 
-export async function getUsers(): Promise<User[]> {
-  const { data } = await api.get('/users');
+type GetUsersResponse = {
+  totalCount: number;
+  users: User[];
+}
 
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        created_at: new Date(user.created_at).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric',
-        })
-      }
-    })
-    return users;
+export async function getUsers(page: number): Promise<GetUsersResponse> {
+  const { data, headers } = await api.get('/users', { 
+    params: {
+      page,
+    }
+});
+
+  const totalCount = Number(headers['x-total-count']);
+
+  const users = data.users.map(user => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: new Date(user.created_at).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      })
+    }
+  })
+  return {users, totalCount};
 }
 
 
-export function useUsers() {
-  return useQuery('users',getUsers , {
+export function useUsers(page: number) {
+  return useQuery(['users', page],() => getUsers(page) , {
     staleTime: 100 * 5,
   })
 }
